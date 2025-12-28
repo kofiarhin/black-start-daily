d8c7d6fe976f3c35c1436b715f867f169ad9d3e3
kofi arhin <kofiarhin69@gmail.com>
2025-12-28 02:13:43 +0000

    add server url

--- client/src/constants/constants.js
+++ client/src/constants/constants.js
@@ -1,1 +1,3 @@
-export const baseUrl = "http://localhost:5000";
+export const baseUrl = import.meta.env.DEV
+  ? "http://localhost:5000"
+  : "https://black-star-daily-api-1967ddff60e6.herokuapp.com";
