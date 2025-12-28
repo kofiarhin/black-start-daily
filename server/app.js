28b86d9509548ee61e03076f7a418b1e9789cd51
kofi arhin <kofiarhin69@gmail.com>
2025-12-28 05:22:01 +0000

    setting up database connection

--- server/app.js
+++ server/app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv").config();
+const connectDB = require("./config/db");
 
 const app = express();
+connectDB();
 
 app.use(
   cors({
