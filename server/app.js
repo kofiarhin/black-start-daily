7be1d2bb87156983c9d516a910fcb1a35e3c715a
kofi arhin <kofiarhin69@gmail.com>
2025-12-28 02:05:48 +0000

    preparing for deployment

--- server/app.js
+++ server/app.js
@@ -1,9 +1,27 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
-app.get("/", async (req, res, next) => {
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
+
+app.get("/", (req, res) => {
   return res.json({ message: "hello world" });
+});
+
+app.get("/api/health", (req, res) => {
+  return res.json({ message: "hello world" });
+});
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
 
 module.exports = app;
